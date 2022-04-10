0377154775f5669019bcca4f39916f68e2492c0d
Ibrahim9942 <89557569+Ibrahim9942@users.noreply.github.com>
2022-04-10 02:04:58 +0200

    Add files via upload

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -1,15 +1,28 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 public class Home extends AppCompatActivity implements View.OnClickListener{
 
-
+    TextView fullName;
+    FirebaseUser fAuth;
+    DatabaseReference fdata;
+    String userId;
     private Button account;
 
     @Override
@@ -17,14 +30,43 @@ public class Home extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        fullName = findViewById(R.id.full_Name);
+        fAuth = FirebaseAuth.getInstance().getCurrentUser() ;
+        assert fAuth != null;
+        userId = fAuth.getUid();
+        fdata = FirebaseDatabase.getInstance().getReference();
+        
         account =  findViewById(R.id.btnAccount);
         account.setOnClickListener(this);
+
+
+
+        DatabaseReference data_ref = FirebaseDatabase.getInstance().getReference("Users/" + userId);
+        data_ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                String val = snapshot.child("fullName").getValue(String.class);
+
+                fullName.setText(val);
+
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
+
 
     @Override
     public void onClick(View view) {
         switch(view.getId()){
-            case R.id.btnAccount: //switches to account page
+            case R.id.btnAccount:
                 startActivity(new Intent(this, Account.class));
                 break;
         }
