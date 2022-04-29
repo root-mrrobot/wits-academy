a681922cd92a0ce477c609622bbabfda79de8644
mzaidally <1828282@students.wits.ac.za>
2022-04-29 16:32:16 +0200

    Adding lecturer view

--- app/src/main/java/com/example/myapplication/LecturerCoursesFragment.java
+++ app/src/main/java/com/example/myapplication/LecturerCoursesFragment.java
@@ -2,63 +2,86 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LecturerCoursesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class LecturerCoursesFragment extends Fragment {
+import com.example.myapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public LecturerCoursesFragment() {
-        // Required empty public constructor
-    }
+public class LecturerCoursesFragment extends Fragment implements View.OnClickListener {
+    ListView myListCourses;
+    ArrayList<String> myArrayList = new ArrayList<>();
+    DatabaseReference coursesRef;
+    View view;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LecturerCoursesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LecturerCoursesFragment newInstance(String param1, String param2) {
-        LecturerCoursesFragment fragment = new LecturerCoursesFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lecturer_courses, container, false);
+        view= inflater.inflate(R.layout.fragment_lecturer_courses, container, false);
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1,myArrayList);
+
+        coursesRef= FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("Lecturer created courses");
+        coursesRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue(String.class);
+                myArrayList.add(value);
+                myArrayAdapter.notifyDataSetChanged();
+                myListCourses.setAdapter(myArrayAdapter);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                myArrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+        myListCourses=(ListView) view.findViewById(R.id.listView);
+
+        return view;
+
     }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
 }
