e10f33e9a8e01457c2a66b468f2739807d83da6f
Fatima-Essa <fatima.ressa@icloud.com>
2022-05-18 16:19:30 +0200

    Got lecturer name using lecID fo student view of courses as well as pop-ups

--- app/src/main/java/com/example/myapplication/StudentHomeFragment.java
+++ app/src/main/java/com/example/myapplication/StudentHomeFragment.java
@@ -78,94 +78,111 @@ public class StudentHomeFragment extends Fragment implements AdapterView.OnItemS
                     String name = child.child("name").getValue().toString();
                     String description = child.child("description").getValue().toString();
                     String category = child.child("courseCategory").getValue().toString();
-                    String lecEmail = child.child("lecEmail").getValue().toString();
+                    String lecID = child.child("lecID").getValue().toString();
                     String image = child.child("imageUri").getValue().toString();
                     float rating = r[new Random().nextInt(r.length)];
 
-                    /* If a category filter is applied, add the courses that are in that category to myArrayList
-                     * Then create a string that contains all the relevant information for each course
-                     * and add each string to the arrayList c
-                     */
-                    if (category.equals(spinner.getSelectedItem().toString())) {
-                        Course course = new Course();
+                    userRef = FirebaseDatabase.getInstance().getReference("Users/" + lecID);
+                    // This fetches the data from firebase
+                    userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            String lecName = snapshot.child("fullName").getValue(String.class);
 
-                        course.setName(name);
-                        course.setDescription((description));
-                        course.setCategory(category);
-                        course.setId(id);
-                        course.setRating(rating);
-                        course.setTeacher(lecEmail);
-                        course.setImage(image);
-                        myArrayList.add(course);
+                            /* If a category filter is applied, add the courses that are in that category to myArrayList
+                             * Then create a string that contains all the relevant information for each course
+                             * and add each string to the arrayList c
+                             */
+                            if (category.equals(spinner.getSelectedItem().toString())) {
+                                Course course = new Course();
 
-                        String singleCourse = "Course Name: " + name + "\nTeacher: " + lecEmail + "\nRating: " + rating;
-                        c.add(singleCourse);
-                    }
-                    /* If the all courses filter is applied, add all the courses to myArrayList
-                     * Then create a string that contains all the relevant information for each course
-                     * and add each string to the arrayList c
-                     */
-                    else if (spinner.getSelectedItem().toString().equals("All Courses")){
-                        Course course = new Course();
 
-                        course.setName(name);
-                        course.setDescription((description));
-                        course.setCategory(category);
-                        course.setId(id);
-                        course.setRating(rating);
-                        course.setTeacher(lecEmail);
-                        course.setImage(image);
-                        myArrayList.add(course);
 
-                        String fullCourse = id + "\n" + name + "\n" + category + "\n" + description + "\n" + rating + "\n" + lecEmail + "\n" + image;
-                        fullC.add(fullCourse);
+                                course.setName(name);
+                                course.setDescription((description));
+                                course.setCategory(category);
+                                course.setId(id);
+                                course.setRating(rating);
+                                course.setTeacher(lecName);
+                                course.setImage(image);
+                                myArrayList.add(course);
 
-                        String singleCourse =  "Course Name: " + name + "\nTeacher: " + lecEmail + "\nRating: " + rating;
-                        c.add(singleCourse);
+                                String singleCourse = "Course Name: " + name + "\nTeacher: " + lecName + "\nRating: " + rating;
+                                c.add(singleCourse);
+                            }
 
-                    }
-                }
-                // display courses using adapter
-                myArrayAdapter.notifyDataSetChanged();
-                myListCourses.setAdapter(myArrayAdapter);
+                            /* If the all courses filter is applied, add all the courses to myArrayList
+                       * Then create a string that contains all the relevant information for each course
+                       * and add each string to the arrayList c
+                      */
+                            else if (spinner.getSelectedItem().toString().equals("All Courses")){
+                                Course course = new Course();
 
-                // open the popup activity and display the course information according to what was clicked on
-                myListCourses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                        // get the course information of the selected course and enter it into an array
-                        String course = fullC.get(position);
-                        String[] splitCourse = course.split(":|\n");
+                                course.setName(name);
+                                course.setDescription((description));
+                                course.setCategory(category);
+                                course.setId(id);
+                                course.setRating(rating);
+                                course.setTeacher(lecName);
+                                course.setImage(image);
+                                myArrayList.add(course);
 
-                        // get the necessary information from the above array
-                        String courseName = splitCourse[1];
-                        //String courseId = splitCourse[];
-                        String category = splitCourse[2];
-                        String description = splitCourse[3];
-                        String rating = splitCourse[4];
-                        String lecturerName = splitCourse[5];
-                        String courseImage = splitCourse[6] + ":"+ splitCourse[7];
+                                String fullCourse = id + "\n" + name + "\n" + category + "\n" + description + "\n" + rating + "\n" + lecName + "\n" + image;
+                                fullC.add(fullCourse);
 
-                        // create a bundle called extras
-                        Bundle extras = new Bundle();
+                                String singleCourse =  "Course Name: " + name + "\nTeacher: " + lecName + "\nRating: " + rating;
+                                c.add(singleCourse);
 
-                        // add all the information that needs to be imported to popup to the extras bundle
-                        extras.putString("courseName", courseName);
-                        extras.putString("courseDescription", description);
-                        extras.putString("category", category);
-                        extras.putString("rating", rating);
-                        extras.putString("lecturerName", lecturerName);
-                        extras.putString("image", courseImage);
+                            }
+
+                            // display courses using adapter
+                            myArrayAdapter.notifyDataSetChanged();
+                            myListCourses.setAdapter(myArrayAdapter);
+
+                            // open the popup activity and display the course information according to what was clicked on
+                            myListCourses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                                    // get the course information of the selected course and enter it into an array
+                                    String course = fullC.get(position);
+                                    String[] splitCourse = course.split(":|\n");
+
+                                    // get the necessary information from the above array
+                                    String courseName = splitCourse[1];
+                                    //String courseId = splitCourse[];
+                                    String category = splitCourse[2];
+                                    String description = splitCourse[3];
+                                    String rating = splitCourse[4];
+                                    String lecturerName = splitCourse[5];
+                                    String courseImage = splitCourse[6] + ":"+ splitCourse[7];
+
+                                    // create a bundle called extras
+                                    Bundle extras = new Bundle();
+
+                                    // add all the information that needs to be imported to popup to the extras bundle
+                                    extras.putString("courseName", courseName);
+                                    extras.putString("courseDescription", description);
+                                    extras.putString("category", category);
+                                    extras.putString("rating", rating);
+                                    extras.putString("lecturerName", lecturerName);
+                                    extras.putString("image", courseImage);
 
                         /* create an intent to go from the current page to the popup and carry over the
                         extras to be used on the popup
                          */
-                        Intent intent = new Intent(view.getContext(), StudentCoursePopUp.class);
-                        intent.putExtras(extras);
-                        startActivity(intent);
-                    }
-                });
+                                    Intent intent = new Intent(view.getContext(), StudentCoursePopUp.class);
+                                    intent.putExtras(extras);
+                                    startActivity(intent);
+                                }
+                            });
+                        }
 
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
             }
 
             @Override
