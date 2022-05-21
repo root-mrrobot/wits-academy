488c81face00bce9a43ded1e2bc5afcb9d55d82b
root-mrrobot <2359325@students.wits.ac.za>
2022-05-21 21:38:58 +0200

    Added Test Cases for Student Home Fragment

---
+++ app/src/androidTest/java/com/example/myapplication/Fragments/StudentHomeFragmentTest.java
@@ -0,0 +1,48 @@
+package com.example.myapplication.Fragments;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
+import android.app.Instrumentation;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.android21buttons.fragmenttestrule.FragmentTestRule;
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.R;
+import com.example.myapplication.StudentHomeFragment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class StudentHomeFragmentTest {
+
+    // Fragment Test Rule for Student Home Fragment
+    @Rule
+    public FragmentTestRule<?, StudentHomeFragment> studentHomeFragmentTestRule = FragmentTestRule.create(StudentHomeFragment.class);
+    StudentHomeFragment studentHomeFragment = studentHomeFragmentTestRule.getFragment();
+
+    // Monitor to be used for Instrumentation
+    Instrumentation.ActivityMonitor monitor = getInstrumentation().addMonitor(MainActivity.class.getName(),null ,false);
+
+//    @Before
+//    public void setUp() throws Exception {
+//    }
+
+    // Checking if the activity appears and whether is matches the id 'studentHomeFragment'
+    @Test
+    public void isActivityInView(){
+        onView(withId(R.id.studentHomeFragment)).check(matches(isDisplayed()));
+    }
+
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+}
