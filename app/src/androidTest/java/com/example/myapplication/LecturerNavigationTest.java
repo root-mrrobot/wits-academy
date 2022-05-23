285fa41b53c74665c58cf1ed7c9f8b6bf7c62c0a
Aashna <2202103@students.wits.ac.za>
2022-05-23 11:11:43 +0200

    Navigation Tests

---
+++ app/src/androidTest/java/com/example/myapplication/LecturerNavigationTest.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class LecturerNavigationTest {
+
+    @Rule
+    public ActivityTestRule<LecturerNavigation> lecturerNavigationTestRule = new ActivityTestRule<>(LecturerNavigation.class);
+
+    @Before
+    public void init(){
+        lecturerNavigationTestRule.getActivity().getSupportFragmentManager().beginTransaction();
+    }
+
+    @Test
+    public void testLaunch(){
+        assertNotNull(lecturerNavigationTestRule.getActivity());
+    }
+
+/*/
+    @Test
+    public void lecturerHome(){
+        lecturerNavigationTestRule.getActivity().findViewById(R.id.lecturerHome).performClick();
+        assertNotNull(lecturerNavigationTestRule.getActivity().getSupportFragmentManager().findFragmentById(R.id.lecturerHomeFragment));
+    }
+*/
+
+    @After
+    public void tearDown(){
+        lecturerNavigationTestRule.getActivity().finish();
+    }
+
+
+
+}
---
+++ app/src/androidTest/java/com/example/myapplication/StudentNavigationTest.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class StudentNavigationTest {
+
+    @Rule
+    public ActivityTestRule<StudentNavigation> studentNavigationActivityRule = new ActivityTestRule<>(StudentNavigation.class);
+
+    @Before
+    public void init(){
+        studentNavigationActivityRule.getActivity().getSupportFragmentManager().beginTransaction();
+    }
+
+    @Test
+    public void testLaunch(){
+        assertNotNull(studentNavigationActivityRule.getActivity());
+    }
+
+    @After
+    public void finish(){
+        studentNavigationActivityRule.getActivity().finish();
+    }
+
+}
