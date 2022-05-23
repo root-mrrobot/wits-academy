95e483023efe2cbe152e4d98524e5ed439769407
root-mrrobot <2359325@students.wits.ac.za>
2022-05-23 06:21:13 +0200

    Removed Failed Test Cases

--- app/src/androidTest/java/com/example/myapplication/LecturerNavigationTest.java
+++ app/src/androidTest/java/com/example/myapplication/LecturerNavigationTest.java
@@ -52,7 +52,7 @@ public class LecturerNavigationTest {
 //    public void isHomeInView(){
 //        onView(withId(R.id.lecturerHome)).check(matches(isDisplayed()));
 //    }
-
+//
     @Test
     public void isCoursesInView(){
         onView(withId(R.id.courses)).check(matches(isDisplayed()));
